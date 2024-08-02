2f19a94e919bb289ed9474890e114c6c1a02c00e
Ava Gonick <144552066+avagonick@users.noreply.github.com>
2024-08-02 15:52:14 -0700

    adding footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import {WebHeader} from './WebHeader'
 import {Home} from './Home'
 import {GetStarted} from './GetStarted'
+import {Footer} from './Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <WebHeader/>
     <Home/>
     <GetStarted/>
+    <Footer/>
     </>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+export function Footer(){
+return (
+<footer>
+    <div id = "FooterContainer">
+        <div><a href = "" target= "_blank" id = "CreditForm">Inquiry Form</a></div>
+        <div className = "Sep">|</div>
+        <div id = "PhoneNumber"> Phone Number</div>
+        <div className = "Sep">|</div>
+        <div id = "MadeBy">Developed by Ava, Lilly, and Max Gonick</div>
+    </div>
+    <div id = "CopyWrite">&#169;2024 Business Class On Points</div>
+</footer>
+    )
+}
