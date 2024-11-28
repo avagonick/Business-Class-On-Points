52b6ac38ca5460f747558330ad921f9f08cc50ea
lilliangonick <lillygonick@gmail.com>
2024-11-27 19:44:44 -0800

    fixed overlapping issue

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -8,7 +8,7 @@ export function NavBar(){
         <div id = "Menu" onClick = {() => {console.log('hi'); setToggled(!Toggled)}}> MENU</div>
         <div id = {Toggled ? "MenuOn" : "MenuOff"}>
             <ul id = "navBar"> 
-                <li><a href = "#Home">Home</a></li>
+                <li><a href = "Home">Home</a></li>
                 <li><a href = "#GetStarted">Get Started</a></li>
                 <li><a href = "#AboutMe">My Story</a></li>
                 <li><a href = "#Gallery">Gallery</a></li>
